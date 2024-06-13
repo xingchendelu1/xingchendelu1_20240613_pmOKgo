972a2a97072bce975f00ec9fbe7663fcb552a50b
星辰德鲁伊 <xingchendelu1@outlook.com>
2024-06-13 20:26:29 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xingchendelu1_20240613_pmOKgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
